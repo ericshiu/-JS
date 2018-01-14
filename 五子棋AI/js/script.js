87afc06792ef8f121c50cc064ab25a953c918fdb
ericshiu <as1102511@gmail.com>
2018-01-14 21:12:56 +0800

    完成AI贏法數組
    完成AI贏法數組

--- 五子棋AI/js/script.js
+++ 五子棋AI/js/script.js
@@ -113,18 +113,16 @@ chess.onclick = function(e) {
   if (over) {
     return;
   }
+  if (!me) {
+    return;
+  }
   var x = e.offsetX;
   var y = e.offsetY;
   var i = Math.floor(x / 30);
   var j = Math.floor(y / 30);
   if (chessBorad[i][j] == 0) {
     oneStep(i, j, me);
-    if (me) {
-      chessBorad[i][j] = 1;
-    } else {
-      chessBorad[i][j] = 2;
-    }
-    me = !me;
+    chessBorad[i][j] = 1;
     for (var k = 0; k < count; k++) {
       if (wins[i][j][k]) {
         myWin[k]++;
@@ -135,8 +133,100 @@ chess.onclick = function(e) {
         }
       }
     }
+    if (!over) {
+      me = !me;
+      computerAI();
+    }
   } else {
     alert('非法定位置');
   }
+};
+var computerAI = function() {
 
+  //黑子分數
+  var myScore = [];
+  //電腦分數
+  var computerScore = [];
+  //最高分數
+  var max = 0;
+  //最高分數的座標
+  var u = 0;
+  var v = 0;
+  for (var i = 0; i < 15; i++) {
+    myScore[i] = [];
+    computerScore[i] = [];
+    for (var j = 0; j < 15; j++) {
+      myScore[i][j] = 0;
+      computerScore[i][j] = 0;
+    }
+  }
+  //跑一次整個棋盤
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 15; j++) {
+      if (chessBorad[i][j] == 0) {
+        //跑片所有贏法
+        for (var k = 0; k < count; k++) {
+          if (wins[i][j][k]) {
+            //判斷黑子目前情況
+            if (myWin[k] == 1) {
+              myScore[i][j] += 200;
+            } else if (myWin[k] == 2) {
+              myScore[i][j] += 400;
+            } else if (myWin[k] == 3) {
+              myScore[i][j] += 2000;
+            } else if (myWin[k] == 4) {
+              myScore[i][j] += 10000;
+            }
+            //判斷電腦目前其況
+            if (computerWin[k] == 1) {
+              computerScore[i][j] += 220;
+            } else if (computerWin[k] == 2) {
+              computerScore[i][j] += 420;
+            } else if (computerWin[k] == 3) {
+              computerScore[i][j] += 2200;
+            } else if (computerWin[k] == 4) {
+              computerScore[i][j] += 20000;
+            }
+          }
+        }
+        if (myScore[i][j] > max) {
+          max = myScore[i][j];
+          u = i;
+          v = j;
+        } else if (myScore[i][j] == max) {
+          if (computerScore[i][j] > computerScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+        if (computerScore[i][j] > max) {
+          max = computerScore[i][j];
+          u = i;
+          v = j;
+        } else if (computerScore[i][j] == max) {
+          if (myScore[i][j] > myScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+      }
+    }
+  }
+  oneStep(u, v, false);
+  chessBorad[u][v] = 2;
+
+  //刷新一次贏法統計數組
+  for (var k = 0; k < count; k++) {
+    if (wins[u][v][k]) {
+      computerWin[k]++;
+      myWin[k] = 6;
+      if (computerWin[k] == 5) {
+        alert('AI贏了');
+        over = true;
+      }
+    }
+  }
+  if (!over) {
+    me = !me;
+  }
 };
